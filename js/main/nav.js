81d85db27ee31b5d8ed253713fd6dce7e2a8e584
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-04-14 14:27:12 -0700

    changes are ready

--- js/main/nav.js
+++ js/main/nav.js
@@ -1,8 +1,11 @@
 // sets the viewport
 function getViewport(view){
     // gets the html from the specific html page associated with the tab clicked
+    console.log("test");
+    
     $.get("viewports/"+view+".html", function (data){
-
+        console.log("gets");
+        
         $('#viewport').html($(data).find('#content').html());
     });
 }
