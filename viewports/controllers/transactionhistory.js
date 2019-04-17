9a9d9e5ff17994db116706cdacf8718a6e1c5730
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-04-16 20:31:00 -0700

    css file is linked to html

--- viewports/controllers/transactionhistory.js
+++
@@ -1,4 +0,0 @@
-$.ajax ({url: "http://blocksandbalancesserver.000webhostapp.com/transactions/transaction-history.php", 
-        success: function (result) {console.log(result)}
-        }
-       )
