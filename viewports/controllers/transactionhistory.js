251adf83cee59ec8c43507f8e03e20adfcce9467
Wesley Monson <wvmonson@gmail.com>
2019-04-16 20:00:21 -0700

    Created transHist.html, CSS folder, and viewports.css

---
+++ viewports/controllers/transactionhistory.js
@@ -0,0 +1,4 @@
+$.ajax ({url: "http://blocksandbalancesserver.000webhostapp.com/transactions/transaction-history.php", 
+        success: function (result) {console.log(result)}
+        }
+       )
