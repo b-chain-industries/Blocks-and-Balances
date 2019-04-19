b3d5d2f7a40a5d859c5d91f12cc280fe7c17746f
Wesley Monson <wvmonson@gmail.com>
2019-04-18 21:52:09 -0700

    more JS for trans history

--- viewports/controllers/transactionhistory.js
+++ viewports/controllers/transactionhistory.js
@@ -3,18 +3,32 @@ function getTransactionHistory(){
         const transHist = JSON.parse(result)
         transHist.forEach(function(result){
 
+            let transResponse1 = result.master1Response
+            let transResponse2 = result.master2Response
+
+            if (transResponse1 == 1) {
+                transResponse1 = `Approved`
+            }
+
+            else {transResponse1 = `Denied`
+            }
+
+            if (transResponse2 == 1) {
+                transResponse2 = `Approved`
+            }
             
+            else {transResponse2 = 'Denied'}
 
             let template = `<div class="pending-holder">
                                 <div class="table">
                                     <div class="amount chart-section">
-                                        `+result.amount+`
+                                        Amount: `+result.amount+`
                                     </div>
                                     <div class="description chart-section">
                                         `+result.description+`
                                     </div>
                                     <div class="status">
-                                        `+result.master1Response+result.master2Responsive+`
+                                        Master1: `+transResponse1+` Master2: `+transResponse2+`
                                     </div>
                                 </div>
                             </div>
