c335e8dd4fa30060546874a4d783a3d3f8a9303b
Gabriel Olmos <gabe_olmos@yahoo.com>
2019-04-23 12:45:27 -0700

    updating to pull, no changes

--- viewports/controllers/masterRequest.js
+++ viewports/controllers/masterRequest.js
@@ -1,24 +1,30 @@
-
+// Fires to make sure everything is in order
 function getMasterPending(){
-    $.get( "http://blocksandbalancesserver.000webhostapp.com/transactions/pending.php", function( data ) { 
+// Sending server info to get correct info back
+    let param = { relationId: user.relation_id }
+    $.post( "http://blocksandbalancesserver.000webhostapp.com/transactions/pending.php", function( data ) { 
+// Turing a String into object, array
     const block = JSON.parse(data);
 
 
-    
+// Forms the pending list, Loop
     block.forEach(function(element){
+// This is the comment section to be filled by templates below        
         let commentTemplate = "";
+// This fills the comment, Loop
         element.comments.forEach(function(comment){
+// comment template
             let temp = 
             `<div class="comment">
                 <span>`+comment.username+`</span><br/>
                 <span>`+comment.comment+`</span>
             </div>`;
-
+// Allows to add more comments below eachother
             commentTemplate += temp;
         });
 
-
-         let template = `<div class="pending-holder">
+// Pending request template
+        let template = `<div class="pending-holder">
          <div class="table pending">
              <div class="amount chart-section">
                  <span>` +element.pendingRequest.amount+ `</span>
@@ -46,6 +52,7 @@ function getMasterPending(){
              </div>
  
      </div>`;
+// Adds the temple to the HTML target
      document.getElementById("master-content").innerHTML += template;
 
     })
