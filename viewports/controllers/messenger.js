0e1171c5a99960c66d3942d3cfd340df8e5670ba
Wesley Monson <wvmonson@gmail.com>
2019-04-23 21:41:10 -0700

    structure for messenger

--- viewports/controllers/messenger.js
+++ viewports/controllers/messenger.js
@@ -1,7 +1,32 @@
 
 function getMessages(){
-    $.get("http://localhost/blocksandbalancesserver/messenger/getmessenges.php?id="+uid, function(data){
-        console.log(data);
-        
+    $.post("http://blocksandbalancesserver.000webhostapp.com/messenger/getMessages.php", {relationId: user.relation_id},
+     function(data){
+        const messages = JSON.parse(data)
+            console.log(messages)
+        messages.forEach(function(result){
+            let loggedInUser = user.id
+            let userTemplate = 
+                `<div class="user">
+                    
+                </div>`
+            let otherUserTemplate = 
+                `<div class="other_user">
+
+                </div>`
+            if (loggedInUser == result.user_id) {
+                document.getElementById("messages").innerHTML += userTemplate;
+            }
+            else{
+                document.getElementById("messages").innerHTML += otherUserTemplate;
+            }
+
+
+            
+
+                        console.log(user)
+                        console.log(result)
+                        // console.log(userMessages)
+        });
     });
 }
--- viewports/controllers/transactionhistory.js
+++ viewports/controllers/transactionhistory.js
@@ -1,24 +1,19 @@
 function getTransactionHistory(){
-    $.ajax({url: "http://blocksandbalancesserver.000webhostapp.com/transactions/transaction-history.php", success: function(result){
+    $.ajax({url: "http://blocksandbalancesserver.000webhostapp.com/transactions/transaction-history.php", success:
+     function(result){
         const transHist = JSON.parse(result)
         transHist.forEach(function(result){
-
             let transResponse1 = result.master1Response
             let transResponse2 = result.master2Response
-
             if (transResponse1 == 1) {
                 transResponse1 = `Approved`
             }
-
             else {transResponse1 = `Denied`
             }
-
             if (transResponse2 == 1) {
                 transResponse2 = `Approved`
             }
-            
             else {transResponse2 = 'Denied'}
-
             let template = `<div class="pending-holder">
                                 <div class="table">
                                     <div class="amount chart-section">
