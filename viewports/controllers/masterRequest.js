b7e1a4fea60ac1abe772fd285ac059f568a9f3ed
Gabriel Olmos <gabe_olmos@yahoo.com>
2019-04-24 13:21:27 -0700

    added JS to Receive and Respond to Request

--- viewports/controllers/masterRequest.js
+++ viewports/controllers/masterRequest.js
@@ -1,12 +1,16 @@
 // Fires to make sure everything is in order
 function getMasterPending(){
+    
+    
 // Sending server info to get correct info back
-    let param = { relationId: user.relation_id }
-    $.post( "http://blocksandbalancesserver.000webhostapp.com/transactions/pending.php", function( data ) { 
+    let param = { 
+        relationId: user.relation_id,
+        userId: user.id 
+    }
+    $.post( "http://blocksandbalancesserver.000webhostapp.com/transactions/getTransactions.php", param, function( data ) { 
+        document.getElementById("master-content").innerHTML = "";
 // Turing a String into object, array
     const block = JSON.parse(data);
-
-
 // Forms the pending list, Loop
     block.forEach(function(element){
 // This is the comment section to be filled by templates below        
@@ -32,12 +36,12 @@ function getMasterPending(){
              <div class="description chart-section">
                  <span>` +element.pendingRequest.description+ `</span>
              </div>
-             <div class="status">
-                 <input type="radio"/>
+             <div id="status">
+                 <input name="requestStatus" type="radio" value="0"/>
                  <span>Approve</span>
-                 <input type="radio"/>
+                 <input name="requestStatus" type="radio" value="1"/>
                  <span>Denied</span>
-                 <button>Submit</button>
+                 <button onclick = "submitRequest(`+element.pendingRequest.request_id+`)" >Submit</button>
              </div>
  
          </div>
@@ -47,8 +51,8 @@ function getMasterPending(){
                  <div class="display-comment">
                      `+commentTemplate+`
                  </div>
-                 <textarea id="addComment" placeholder="Write your comment"></textarea>
-                 <button>Submit</button>
+                 <textarea id="addComment`+element.pendingRequest.request_id+`" placeholder="Write your comment"></textarea>
+                 <button onclick = "masterRequest(`+element.pendingRequest.request_id+`)">Submit</button>
              </div>
  
      </div>`;
@@ -57,11 +61,54 @@ function getMasterPending(){
 
     })
 
-
-   
-    
-
-
 });
 }
 
+
+
+
+
+// Function that submits final request
+function submitRequest(x){
+    
+    // variable to name which button was pushed
+    let status = "";
+    // labels and determines which buttons are pushed
+    let radioBtn = document.getElementsByName("requestStatus");
+    for (var i = 0; i < radioBtn.length; i++){
+        if (radioBtn[i].checked){
+            status = radioBtn[i].value;
+        }
+    }
+
+    // tells server who, what and where the request/approval will go
+    let param = {
+        userId: user.id,
+        request_id: x,
+        approved: status,
+    
+    }
+    
+    
+}
+
+
+// Comments from the master to child
+function masterRequest(x){
+    // adds the new comment is the comment box
+   var comment = document.getElementById('addComment'+x).value;
+    // tells who, where, and what the comment is saying
+   var params = {
+       requestId:x,
+       username:user.username,
+       comment:comment,
+   }
+    //AJAX call to the server to move and transfer the data  
+   $.post('http://blocksandbalancesserver.000webhostapp.com/transactions/addComment.php', params, function (data){
+        
+       getMasterPending();
+       
+   })
+
+
+}
