1e170fab2aad0692e73f2c38bec9060df5ed3fc3
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-05-07 20:37:28 -0700

    black btn

--- js/main/init.js
+++ js/main/init.js
@@ -58,6 +58,7 @@ $.post("http://blocksandbalancesserver.000webhostapp.com/user/getUser.php", {id:
 function initNav(status){
     if (status == 0) {
         statusNav.innerHTML =
+<<<<<<< HEAD
         `<div id="initTab" class="tab" onclick="getViewport('childRequest')"><i class="fas fa-exchange-alt"></i>Requests</div>
         <div class="tab" onclick="getViewport('transHist')"><i class="fas fa-chart-bar"></i>Transaction History</div>
          <div class="tab" onclick="getViewport('messenger')"><i class="fas fa-comment"></i>Messenger</div>`;
@@ -66,5 +67,15 @@ function initNav(status){
         `<div id="initTab" class="tab" onclick="getViewport('masterRequest')"><i class="fas fa-exchange-alt"></i>Requests</div>
         <div class="tab" onclick="getViewport('transHist')"><i class="fas fa-chart-bar"></i>Transaction History</div>
          <div class="tab" onclick="getViewport('messenger')"><i class="fas fa-comment"></i>Messenger</div>`;
+=======
+        `<div id="initTab" class="tab" onclick="getViewport('childRequest')">Requests</div>
+        <div class="tab" onclick="getViewport('transHist')">Transaction History</div>
+         <div class="tab" onclick="getViewport('messenger')">Messenger</div>`;
+    } else {
+        statusNav.innerHTML =
+        `<div id="initTab" class="tab" onclick="getViewport('masterRequest')">Requests</div>
+        <div class="tab" onclick="getViewport('transHist')">Transaction History</div>
+         <div class="tab" onclick="getViewport('messenger')">Messenger</div>`;
+>>>>>>> parent of a5fe209... navigation btn
     }
 }
