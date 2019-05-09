3b897cdedfa09f4f54d9bdb29ec0427d32c007b8
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-05-09 12:42:24 -0700

    ready for pull request

--- js/main/init.js
+++ js/main/init.js
@@ -82,7 +82,7 @@ function initNav(status){
          </div>`;
     } else {
         statusNav.innerHTML =
-        `<div id="initTab" class="tab" onclick="getViewport('masterRequest')"><i class="fas fa-exchange-alt"></i>Requests</div>
+        `<div id="initTab" class="tab" onclick="getViewport('masterRequest')><i class="fas fa-exchange-alt"></i>Requests</div>
         <div class="tab" onclick="getViewport('transHist')"><i class="fas fa-chart-bar"></i>Transaction History</div>
          <div class="tab" onclick="getViewport('messenger')"><i class="fas fa-comment"></i>Messenger</div>
          <div  class="tab" ><i class="fab fa-vimeo-v fas"></i>Overview</div>
