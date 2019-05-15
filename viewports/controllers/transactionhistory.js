b23afe1ddff97eef980ec4374bd4cc89c78e774b
Wesley Monson <wvmonson@gmail.com>
2019-05-14 20:39:32 -0700

    added template for when tehre is no transaction history yet it will dispaly as such

--- viewports/controllers/transactionhistory.js
+++ viewports/controllers/transactionhistory.js
@@ -5,26 +5,32 @@ function getTransactionHistory(){
     $.post("http://blocksandbalancesserver.000webhostapp.com/transactions/getTransHist.php", params,
      function(result){
         const transHist = JSON.parse(result)
-        transHist.forEach(function(result){
-            var transHistClass = result.approved
-            if (transHistClass == 1) {
-                transHistClass = `histApproved`
+            let noHistTemplate = `<div><h2>No transactions have been made yet.</h2></div>`
+            if (result.length === 0){
+                document.getElementById("trans_content").innerHTML += noHistTemplate;
             }
             else {
-                transHistClass = `histDenied` 
+                transHist.forEach(function(result){
+                    var transHistClass = result.approved
+                    if (transHistClass == 1) {
+                        transHistClass = `histApproved`
+                    }
+                    else {
+                        transHistClass = `histDenied` 
+                    }
+                    let template = `<div class="pending-holder ` +transHistClass+ `">
+                                        <div class="table">
+                                            <div class="amount chart-section">
+                                                Amount: `+result.amount+`
+                                            </div>
+                                            <div class="description wchart-section">
+                                                `+result.description+`
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="border"></div>`
+                    document.getElementById("trans_content").innerHTML += template;
+                });
             }
-            let template = `<div class="pending-holder ` +transHistClass+ `">
-                                <div class="table">
-                                    <div class="amount chart-section">
-                                        Amount: `+result.amount+`
-                                    </div>
-                                    <div class="description wchart-section">
-                                        `+result.description+`
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="border"></div>`
-            document.getElementById("trans_content").innerHTML += template;
-        });
     });   
 }
