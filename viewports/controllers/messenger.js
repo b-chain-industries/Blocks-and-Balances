50b9dd168fa2db7778a9ed8b8145ea486907c2ff
Dylan Mahaffey <dylan.mahaffey@gmail.com>
2019-05-16 15:12:05 -0700

    only update messager if there is no messages

--- viewports/controllers/messenger.js
+++ viewports/controllers/messenger.js
@@ -2,41 +2,43 @@
 function getMessages(){
     $.post("http://blocksandbalancesserver.000webhostapp.com/messenger/getMessages.php", {relationId: user.relation_id}, function(data){
         document.getElementById("messages").innerHTML = "";
-        const messages = JSON.parse(data)
-        messages.forEach(function(result){
-            let loggedInUser = user.ID
-            let messageText = result.message
-            let timeStamp = result.timestamp
-            
-            let userTemplate = `<div class="user">
-                                    <div class="message_text">`
-                                        +messageText+
-                                    `</div>
-                                    <div class="time_stamp">`
-                                        +timeStamp+
-                                    `</div>
-                                </div>`
-            let otherUserTemplate = `<div class="other_user">
+        if(JSON.parse(data) !== messages){
+            messages = JSON.parse(data)
+            messages.forEach(function(result){
+                let loggedInUser = user.ID
+                let messageText = result.message
+                let timeStamp = result.timestamp
+                
+                let userTemplate = `<div class="user">
                                         <div class="message_text">`
                                             +messageText+
-                                        `</div>               
-                                        <div class="time_stamp">`+timeStamp+
+                                        `</div>
+                                        <div class="time_stamp">`
+                                            +timeStamp+
                                         `</div>
                                     </div>`
-            if (loggedInUser == result.user_id) {
-             document.getElementById("messages").innerHTML += userTemplate;
-            }
-            else{
-                document.getElementById("messages").innerHTML += otherUserTemplate;
-            }
-        });
-
-
-        function scrollBottom (messages) {
-            var div = document.getElementById(messages);
-            div.scrollTop = div.scrollHeight - div.clientHeight;
-        }        
-        scrollBottom("messages");
+                let otherUserTemplate = `<div class="other_user">
+                                            <div class="message_text">`
+                                                +messageText+
+                                            `</div>               
+                                            <div class="time_stamp">`+timeStamp+
+                                            `</div>
+                                        </div>`
+                if (loggedInUser == result.user_id) {
+                 document.getElementById("messages").innerHTML += userTemplate;
+                }
+                else{
+                    document.getElementById("messages").innerHTML += otherUserTemplate;
+                }
+            });
+    
+    
+            function scrollBottom (messages) {
+                var div = document.getElementById(messages);
+                div.scrollTop = div.scrollHeight - div.clientHeight;
+            }        
+            scrollBottom("messages");
+        }
     });
     
 }
